139057f9940c3eb80044bb568a78b62a28376a9a
Guillermo Julián <gjulian93@gmail.com>
2014-03-11 14:09:51 +0100

    Basura

--- workdir/pra2/pract2a.asm
+++ workdir/pra2/pract2a.asm
@@ -8,7 +8,7 @@
 
 DATOS SEGMENT 
 
-MATRIZ DB 9 DUP (0)
+MATRIZ DB 9 DUP (1)
 
 DATOS ENDS 
 
@@ -49,78 +49,39 @@ START PROC
 	MOV AX, EXTRA
 	MOV ES, AX
 	MOV SP, 64 ; CARGA EL PUNTERO DE PILA CON EL VALOR MAS ALTO
+
+	MOV DI, 0H
+	CALL DT_DIAG
 START ENDP 
 
-DT_R  PROC NEAR
-	; Entradas:
-	; 	Ninguna.
-	; Salidas:
-	; 	Determinante: 	BX
-	MOV DI, 2H
-	XOR BX, BX
-	MOV CH, 1
+DT_DIAG PROC NEAR
+	MOV AX, 1H
+	MOV SI, 0H
 
-DT_R_L1:
+DT_DIAG_LOOP:
+	MOV BL, MATRIZ[DI]
+
+	MUL BL
+	ADD DI, 4H
+
+;; Módulo 9	
+	PUSH AX
+	MOV AX, DI
+	MOV DX, 9H
+	DIV DX
 	MOV DX, DI
-	MOV CL, DL
-	CALL DT_SR
-	ADD BX, AX
-
-	DEC DI
-	JNE DT_R_L1
-
-	CMP CH, -1H
-	JE DT_END
-
-	MOV DI, 2H
-	MOV CH, -1H
-	JMP DT_R_L1
-
-DT_END:
-	RET
-DT_R ENDP
-
-DT_SR PROC NEAR 
-	; Entradas:
-	; 	Contador columna: 		BX
-	; 	Tipo de producto: 		CX
-	; Salidas:
-	; 	Producto "diagonal": 	AX
-	MOV AX, 1H ; Inicializamos producto = 1 en AX
-	MOV SI, 0H ; Primera fila
-
-IR: 
-	MOV AX, SI
-	MOV DX, 3
-	MUL DX
-	ADD DX, BX
-	MOV SI, DX
-
 	POP AX
-	XOR DH, DH
-	MOV DL,MATRIZ[SI]
-	MUL DX
-	PUSH AX    ; Guardamos AX para no sobreescribir.
+;; Módulo 9 fin
 
-	; Adición módulo tres al contador de columna.
-	ADD BX, CX ; Sumamos type en el contador de columna.
-	ADD AX, BX ; Guardamos en AX el valor de contador de columna.
-	ADD AX, 3  ; Sumamos tres a AX para evitar el caso en el que sea negativo.
-	MOV DX, 3 	
-	DIV DX	   ; Dividimos el contador de columna (al que hemos sumado tres) por tres.
-	MOV CL, AL ; En AL está el resto de la división, es decir, CL módulo 3.
-	XOR CH, CH ; El valor de CL módulo 3 será siempre positivo, los bytes más significativos son 0.
-	; Fin adición módulo tres.
-
-	INC SI     ; Incrementados el contador de fila.
-	CMP SI, 3H ; Miramos si es igual a 3.
-	JNE IR     ; Si no hemos llegado al final del bucle, reiteramos.
-	; Fin bucle.
-
-	IMUL CH ; Multiplicamos el resultado por +1 ó -1 según sea el orden de multiplicación.
+	INC SI
+	CMP SI, 3H
+	JNE DT_DIAG_LOOP
 
 	RET
-DT_SR ENDP 
+DT_DIAG ENDP
+
+MD PROC NEAR
+	
 
 CODE ENDS
 END START
