bab9522b6289995b3c758d6b0043c9f01f976ee2
VicdeJuan <dejuan.victor@gmail.com>
2014-03-17 15:08:42 +0100

    Ya imprime bien los negativos y calcula el determinante salvo overflows en mi opinión incontrolables

--- workdir/pra2/pract2a.asm
+++ workdir/pra2/pract2a.asm
@@ -7,12 +7,13 @@
 ; DEFINICION DEL SEGMENTO DE DATOS 
 
 DATOS SEGMENT 
-	MATRIZ DB 3,-2,-3,0,-6,-2,10,12,-1,"$"
+	MATRIZ DB -3,-2,-3,0,-6,-2,10,12,-1,"$"
 	TEXTO_INI DB "La matriz 3x3 es:",0AH,"$"
 	DEC_RESULT DB 50 DUP(0)
 	BUFFEREND DB "$"
-	DET_RESULT DW 0,0,"$"
+	DET_RESULT DB 6 DUP (0),"$"
 	TEXTO_FIN DB 0AH,"El determinante es:",0AH,"$"
+	AUX DW
 
 DATOS ENDS 
 
@@ -59,7 +60,7 @@ START PROC
 	MOV DI, 9H
 	MOV SI, OFFSET BUFFEREND
 	MOV BX, OFFSET MATRIZ
-	CALL PRINT
+	CALL PRINT_BYTES
 
 	MOV AH,9H
 	MOV CL,CH
@@ -102,27 +103,26 @@ MAIN_LOOP_2:
 	CMP SI,3H
 	JNE MAIN_LOOP_2
 
-	;; Tenemos el resultado en BX. Ahora vamos a imprimirlo.
+	;;	YA HEMOS CALCULADO EL DETERMINANTE.  
+	;;	Tenemos el resultado en BX. Ahora vamos a imprimirlo.
 
 
-	;; Argumentos de entrada de print para convertirlo a ASCII.
-	MOV SI,OFFSET DET_RESULT
+	;; Argumentos de entrada de print_BYTES para convertirlo a ASCII.
+	MOV SI,OFFSET AUX
 	MOV [SI],BX
-	MOV BX, OFFSET DET_RESULT
+	MOV BX, OFFSET AUX
 	MOV SI, OFFSET TEXTO_FIN-2
 	MOV DI, 1H
-	CALL PRINT
+	CALL PRINT_WORDS
 
 
 	;;IMPRIMIMOS EL TEXTO.
 	MOV AH,9H
 	MOV DX, OFFSET TEXTO_FIN
 	INT 21H
+
 	;; IMPRIMIMOS EL VALOR DEL DETERMINANTE.
-	MOV CL,CH
-	XOR CH,CH
-	MOV DX,OFFSET TEXTO_FIN-2
-	SUB DX,CX
+	MOV DX,OFFSET DET_RESULT
 	INT 21H
 
 	MOV AX, 4C00H 
@@ -131,58 +131,6 @@ MAIN_LOOP_2:
 START ENDP 
 
 
-
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;PRINT PROCEDURE;;;;;;;;;;;;;;;;;;;;;;
-PRINT PROC NEAR
-;; Parámetros: 
-;	IN: 	BX:		El offset de donde está el valor.
-;			SI: 	El offset de la última posición en la que se empieza a escribir.
-;			DI: 	El número de caracteres a imprimir.
-;
-;	OUT:	Almacena en [SI] los bytes ASCII de los caracteres del número.
-;	
-;	USES: AX,BX,CL,CH,DL,DI,SI 
-	MOV DL,1H
-	XOR CH,CH
-	MOV CL,0AH
-MAIN:
-	MOV AL,[BX+DI-1]
-	MOV AH,AL
-	AND AH,80H
-	JZ  NEG_CORRECTED
-	NEG AL
-	MOV DL,0H
-NEG_CORRECTED:
-CONVERT:
-    XOR AH,AH
-	DIV CL
-	ADD AH,'0'
-	DEC SI
-	MOV [SI],AH
-	INC CH
-	AND AL, AL
-	JNZ CONVERT
-	DEC DI
-	JZ LAST
-	CMP DL,0H
-	JNZ NO_MINUS
-	MOV DL,1H
-	INC DL
-	INC CH
-	DEC SI
-	MOV [SI],BYTE PTR '-'
-NO_MINUS:	
-	DEC SI
-	INC CH
-	MOV [SI],BYTE PTR ','
-LAST:
-	CMP DI,0H
-	JNZ MAIN
-	RET
-PRINT ENDP
 
 
 
@@ -220,6 +168,109 @@ DT_DIAG_LOOP:
 
 	RET
 DT_DIAG ENDP
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;PRINT PROCEDURE;;;;;;;;;;;;;;;;;;;;;;
+PRINT_BYTES PROC NEAR
+;; Parámetros: 
+;	IN: 	BX:		El offset de donde está el valor.
+;			SI: 	El offset de la última posición en la que se empieza a escribir.
+;			DI: 	El número de caracteres a imprimir.
+;
+;	OUT:	Almacena en [SI] los bytes ASCII de los caracteres del número.
+;	
+;	USES: AX,BX,CL,CH,DL,DI,SI 
+	MOV DL,1H
+	XOR CH,CH
+	MOV CL,0AH
+MAIN_BYTE:
+	MOV AL,[BX+DI-1]
+	MOV AH,AL
+	AND AH,80H
+	JZ  NEG_CORRECTED_BYTE
+	NEG AL
+	MOV DL,0H
+NEG_CORRECTED_BYTE:
+CONVERT_BYTE:
+    XOR AH,AH
+	DIV CL
+	ADD AH,'0'
+	DEC SI
+	MOV [SI],AH
+	INC CH
+	AND AL, AL
+	JNZ CONVERT_BYTE
+	CMP DL,0H
+	JNZ NO_MINUS_BYTE
+	MOV DL,1H
+	INC DL
+	INC CH
+	DEC SI
+	MOV [SI],BYTE PTR '-'
+NO_MINUS_BYTE:	
+	DEC DI
+	JZ LAST_BYTE
+
+	DEC SI
+	INC CH
+	MOV [SI],BYTE PTR ','
+LAST_BYTE:
+	CMP DI,0H
+	JNZ MAIN_BYTE
+	RET
+PRINT_BYTES ENDP
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;PRINT PROCEDURE;;;;;;;;;;;;;;;;;;;;;;
+PRINT_WORDS PROC NEAR
+;; Parámetros: 
+;	IN: 	BX:		El offset de donde está el valor.
+;			SI: 	El offset de la última posición en la que se empieza a escribir.
+;			DI: 	El número de caracteres a imprimir.
+;
+;	OUT:	Almacena en [SI] los bytes ASCII de los caracteres del número.
+;	
+;	USES: AX,BX,CL,CH,DL,DI,SI 
+	MOV DL,1H
+	MOV CL,0AH
+MAIN:
+	MOV AX,[BX+DI-1]
+	JS  NEG_CORRECTED
+	NEG AX
+	MOV DL,0H
+NEG_CORRECTED:
+CONVERT:
+    XOR AH,AH
+	DIV CL
+	ADD AH,'0'
+	DEC SI
+	MOV [SI],AH
+	INC CH
+	AND AL, AL
+	JNZ CONVERT_BYTE
+	DEC DI
+	JZ LAST_BYTE
+	CMP DL,0H
+	JNZ NO_MINUS_BYTE
+	MOV DL,1H
+	INC DL
+	INC CH
+	DEC SI
+	MOV [SI],BYTE PTR '-'
+NO_MINUS:	
+	DEC SI
+	INC CH
+
+	MOV [SI],BYTE PTR ','
+LAST:
+	CMP DI,0H
+	JNZ MAIN
+	RET
+PRINT_WORDS ENDP
+
+
+
 
 CODE ENDS
 END START
