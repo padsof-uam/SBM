5e602c1ffc57520dea4d0f4667d0764d39ecbf91
VicdeJuan <dejuan.victor@gmail.com>
2014-03-17 22:43:34 +0100

    Venga ya si que sí. Autores en la cabecera

--- workdir/P2_G_V/pract2a.asm
+++ workdir/P2_G_V/pract2a.asm
@@ -1,8 +1,7 @@
 ;************************************************************************** 
-
-; ESTRUCTURA BÁSICA DE UN PROGRAMA EN ENSAMBLADOR 
-
+;	Autores: Víctor de Juan Sanz y Guillermo Julián Moreno.
 ;************************************************************************** 
+
 
 ; DEFINICION DEL SEGMENTO DE DATOS 
 
@@ -17,23 +16,19 @@ DATOS SEGMENT
 DATOS ENDS 
 
 ;************************************************************************** 
-
 ; DEFINICION DEL SEGMENTO DE PILA 
 
 PILA SEGMENT STACK "STACK" 
-	DB 40H DUP (0) ;ejemplo de inicialización, 64 bytes inicializados a 0 
+	DB 40H DUP (0) 
 PILA ENDS 
 
 ;************************************************************************** 
-
 ; DEFINICION DEL SEGMENTO EXTRA 
 
 EXTRA SEGMENT 
-
 EXTRA ENDS 
 
 ;************************************************************************** 
-
 ; DEFINICION DEL SEGMENTO DE CODIGO 
 
 CODE SEGMENT 
