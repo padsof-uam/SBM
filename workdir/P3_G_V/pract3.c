47a224839c30fd4956bc038659c2f53e0ab8d31c
VicdeJuan <dejuan.victor@gmail.com>
2014-03-18 13:52:54 +0100

    Listo para empezar la practica 3

---
+++ workdir/P3_G_V/pract3.c
@@ -0,0 +1,15 @@
+
+
+unsigned int minimoComunMultiplo (unsigned int a, unsigned int b);
+
+
+int calculaMediana(int a, int b, int c, int d);
+
+int esFibonacci(unsigned int num);
+
+int divisiblePor4(int num);
+
+int main(int argc, char const *argv[])
+{
+	return 0;
+}
