61ef46b88bec244ce2696d15087e7310a2de51af
VicdeJuan <dejuan.victor@gmail.com>
2014-04-01 14:13:26 +0200

    Commit inicial

--- workdir/P4_G_V/p4b.asm
+++ workdir/P4_G_V/p4b.asm
@@ -1,27 +1,18 @@
 ;************************************************************************** 
-;Víctor de Juan Sanz y Guillermo Julián Moreno
+;	Pareja 3
+;	Víctor de Juan Sanz y Guillermo Julián Moreno
 ;************************************************************************** 
 
-; DEFINICION DEL SEGMENTO DE DATOS 
 DATOS SEGMENT 
-;-- rellenar con los datos solicitados 
 DATOS ENDS 
 
-;************************************************************************** 
-; DEFINICION DEL SEGMENTO DE PILA 
 PILA SEGMENT STACK "STACK" 
 	DB 40H DUP (0) ;ejemplo de inicialización, 64 bytes inicializados a 0 
 PILA ENDS 
 
-;************************************************************************** 
-; DEFINICION DEL SEGMENTO EXTRA 
-
 EXTRA SEGMENT 
-	RESULT DW 0,0 ;ejemplo de inicialización. 2 PALABRAS (4 BYTES) 
 EXTRA ENDS 
 
-;************************************************************************** 
-; DEFINICION DEL SEGMENTO DE CODIGO 
 
 CODE SEGMENT 
 	ASSUME CS: CODE, DS: DATOS, ES: EXTRA, SS: PILA 
