7416494238f81be41728b2f0f3ae47f91854b30d
VicdeJuan <dejuan.victor@gmail.com>
2014-04-01 13:56:30 +0200

    commit inicial Practica4

---
+++ workdir/P4_G_V/p4a.asm
@@ -0,0 +1,37 @@
+;************************************************************************** 
+;Víctor de Juan Sanz y Guillermo Julián Moreno
+;************************************************************************** 
+
+; DEFINICION DEL SEGMENTO DE DATOS 
+DATOS SEGMENT 
+;-- rellenar con los datos solicitados 
+DATOS ENDS 
+
+;************************************************************************** 
+; DEFINICION DEL SEGMENTO DE PILA 
+PILA SEGMENT STACK "STACK" 
+	DB 40H DUP (0) ;ejemplo de inicialización, 64 bytes inicializados a 0 
+PILA ENDS 
+
+;************************************************************************** 
+; DEFINICION DEL SEGMENTO EXTRA 
+
+EXTRA SEGMENT 
+	RESULT DW 0,0 ;ejemplo de inicialización. 2 PALABRAS (4 BYTES) 
+EXTRA ENDS 
+
+;************************************************************************** 
+; DEFINICION DEL SEGMENTO DE CODIGO 
+
+CODE SEGMENT 
+	ASSUME CS: CODE, DS: DATOS, ES: EXTRA, SS: PILA 
+START PROC
+
+
+	MOV AX, 4C00H 
+    INT 21H 
+
+START ENDP 
+
+CODE ENDS
+END START
